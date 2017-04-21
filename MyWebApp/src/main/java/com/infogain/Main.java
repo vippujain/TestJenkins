c22d4c4519721c61168832ccc04f232e4165e84a
vippujain <vippujain@gmail.com>
2017-04-21 21:44:57 +0530

    edit in Main to check Jenkins Job

--- MyWebApp/src/main/java/com/infogain/Main.java
+++ MyWebApp/src/main/java/com/infogain/Main.java
@@ -7,7 +7,7 @@ public class Main {
 		System.out.println(emp);
 		System.out.println(emp2);
 		
-		Manager manager=new Manager("Jacob","Insurance");
+		Manager manager=new Manager("Vipul","Insurance");
 		System.out.println(manager);
 	}
 
